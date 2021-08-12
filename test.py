4bbe6fe7c7da26236cb6e7f52ab2afb6e331d854
Deep Patel <deep.researchnode@gmail.com>
2021-08-12 18:20:20 +0530

    get present and absent list

--- test.py
+++ test.py
@@ -1,22 +1,42 @@
 import csv
+import time
 
 roll_list=[ "MSM19B001","MSM19B002","MSM19B003","MSM19B004","MSM19B005","MSM19B006","MSM19B008","MSM19B009","MSM19B010","MSM19B011","MSM19B012","MSM19B013","MSM19B014",
 "MSM19B015","MSM19B017","MSM19B018","MSM19B019","MSM19B020","MSM19B021","MSM19B023","MSM19B024","MSM19B025","MSM19B028","MSM19B030","MSM19B031","MSM19B032","MSM19B033",
 "MSM19B034","MSM19B035","MSM19B036","MSM19B037","MSM19B038","MSM19B039","MSM19B040","MSM19B041","MSM19B042","MSM19B043","MSM19B044","MSM19B045","MSM19B046","MSM18B031"]
-
-print(roll_list)
+atl=[]
+p_list=[]
+a_list=[]
 with open('sample.csv', 'r') as file:
     reader = csv.reader(file)
+         
     for row in reader:
-          fw=row[0].split()[0]
-          #print(fw)
-          if roll_list[2] == fw:
-           print(row)
+         if row[0].startswith('MSM'):
+             fw=row[0].split()[0]
+             atl.append(fw)
+             print(fw)
+         # if roll_list[2] == fw:
+          # print(row)
+             #if (fw in roll_list):
+             time = row[2] 
+             du=sum(x * int(t) for x, t in zip([3600, 60, 1], time.split(":"))) 
+             if(du>3000*0.70):
+              p_list.append(fw)
+             else: 
+              a_list.append(fw)
+    print(atl)
+    print("=============Present seperatiom=================")
+    a_list=a_list+list(set(roll_list)-set(atl))
+    print(p_list)
+    print("=============Abbesnt seperatiom=================")
+    print(a_list)
+
+            
 
 with open('main.csv', 'r') as file:
     reader_m = csv.reader(file)
-    for row in reader_m:
-         if row[2].startswith('MSM'):
-            print(row[2])
+     #for row in reader_m:
+          #if row[2].startswith('MSM'):
+            # print(row[2])
 
 
