6a2cc595212db88ed1f11c7061760361dc5dae4c
Deep Patel <deep.researchnode@gmail.com>
2021-08-12 12:42:00 +0530

    search for defined roll inboth csv

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import csv
+
+search_string = "MSM18B031"
 with open('sample.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
+        fw=row[0].split()[0]
+        #print(fw)
+        if search_string == fw:
+         print(row)
 
 with open('main.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
+        if search_string == row[2]:
+         print(row)
+
 
