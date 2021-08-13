4d4cb535b6170a8d18375a56f2e01048c99d24a2
Deep Patel <deep.researchnode@gmail.com>
2021-08-13 13:01:41 +0530

    add input file for attent update in main

--- take-attendance.py
+++ take-attendance.py
@@ -2,7 +2,12 @@ import csv
 import tempfile
 import time
 from tempfile import NamedTemporaryFile
-
+import glob
+import os
+import shutil
+name=""
+for name in glob.glob('/home/deep/take-attendance/input/*csv'):
+    print(name)
 roll_list=[ "MSM19B001","MSM19B002","MSM19B003","MSM19B004","MSM19B005","MSM19B006","MSM19B008","MSM19B009","MSM19B010","MSM19B011","MSM19B012","MSM19B013","MSM19B014",
 "MSM19B015","MSM19B017","MSM19B018","MSM19B019","MSM19B020","MSM19B021","MSM19B023","MSM19B024","MSM19B025","MSM19B028","MSM19B030","MSM19B031","MSM19B032","MSM19B033",
 "MSM19B034","MSM19B035","MSM19B036","MSM19B037","MSM19B038","MSM19B039","MSM19B040","MSM19B041","MSM19B042","MSM19B043","MSM19B044","MSM19B045","MSM19B046","MSM18B031"]
@@ -10,7 +15,7 @@ atl=[]
 p_list=[]
 a_list=[]
 empty=[]
-with open('sample.csv', 'r') as file:
+with open(name, 'r') as file:
     reader = csv.reader(file)
          
     for row in reader:
@@ -63,6 +68,7 @@ print("=========================================")
 print(empty)                  
 with open('main.csv', 'w') as file:
       writer_m = csv.writer(file)
-      writer_m.writerows(empty)                    
+      writer_m.writerows(empty)   
+shutil.move(name, "/home/deep/take-attendance/backup/old.csv")                       
 
 
